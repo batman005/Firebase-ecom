6cf68cff0d067a19b433e72db2120f43e6da7006
HRITHIK GOSWAMI <51878340+batman005@users.noreply.github.com>
2022-06-01 17:38:47 +0530

    Deploying build to netlify

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -9,7 +9,7 @@ import {
   signOut,
   onAuthStateChanged
 } from 'firebase/auth';
-import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
+import { getFirestore, doc, getDoc, setDoc, collection, writeBatch, query, getDocs } from 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: "AIzaSyCZ5TErVbzI5tetFpoDqXKKvt2j-XGcegs",
@@ -20,7 +20,7 @@ const firebaseConfig = {
   appId: "1:941506690737:web:53cf5b49d2434aca08ac90"
 };
 
-const firebaseApp = initializeApp(firebaseConfig);
+export const firebaseApp = initializeApp(firebaseConfig);
 
 const googleProvider = new GoogleAuthProvider();
 
@@ -35,6 +35,36 @@ export const signInWithGoogleRedirect = () =>
   signInWithRedirect(auth, googleProvider);
 
 export const db = getFirestore();
+
+export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => {
+     const collectionRef = collection(db, collectionKey);
+     const batch = writeBatch(db);
+
+     objectsToAdd.forEach((object) => {
+        const documentRef = doc(collectionRef, object.title.toLowerCase());
+        batch.set(documentRef, object);
+ });
+
+ await batch.commit();
+ console.log('done');
+}
+
+
+export const getCategoriesAndDocuments = async () => {
+  const collectionRef = collection(db, 'categories');
+  const q = query(collectionRef);
+
+  const querySnapshot = await getDocs(q);
+  const categoryMap = querySnapshot.docs.reduce((acc, docSnapshot) => {
+     const {title, items} = docSnapshot.data();
+      acc[title.toLowerCase()] = items;
+      return acc;
+  }, {});
+
+  return categoryMap;
+}
+
+
 
 export const createUserDocumentFromAuth = async (
   userAuth,
